2bcc61aa102a8bcd66f0215d5fb74c0ff61f0665
Dmitry Stillermann <dmitry@stillermann.com>
2020-06-10 20:46:34 +0300

    v0.0.7

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 import setuptools
+import re
 
-with open('README.md', 'r') as fh:
-    long_description: str = fh.read()
 
-with open('requirements.txt', 'r') as fh:
-    install_requires: list = [line.strip() for line in fh]
+def _read_long_description() -> str:
+    with open('README.md', 'r') as fh:
+        return fh.read()
+
+
+def _read_install_requires() -> list:
+    with open('requirements.txt', 'r') as fh:
+        return [line.strip() for line in fh]
+
+
+def _read_version() -> str:
+    ver_file: str = 'piggypandas/_version.py'
+    with open(ver_file, 'rt') as fh:
+        text: str = fh.read()
+        m = re.search(r'__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]', text, re.MULTILINE)
+        if m:
+            return m.group(1)
+        else:
+            raise RuntimeError(f"Unable to locate version string in \"{ver_file}\"")
+
 
 setuptools.setup(
     name="piggypandas",
-    version="0.0.6",
+    version=_read_version(),
     author="Dmitry Stillermannm",
     author_email="dmitry@stillermann.com",
     description="A few helpers for more efficient pandas work",
-    long_description=long_description,
+    long_description=_read_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/dstillermann/piggypandas",
     packages=setuptools.find_packages(),
@@ -25,5 +42,5 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     python_requires='>=3.6',
-    install_requires=install_requires
+    install_requires=_read_install_requires()
 )
